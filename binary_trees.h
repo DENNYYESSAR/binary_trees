0ff7c22c43f1f662dbc6be2b7345432df12fe093
DENNYYESSAR <dennisokeri5@gmail.com>
2024-03-26 01:58:03 -0700

    New node

--- binary_trees.h
+++ binary_trees.h
@@ -22,6 +22,17 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
+/**
+ * struct levelorder_queue_s - Level order traversal queue.
+ * @node: A node of a binary tree.
+ * @next: The next node to traverse to in the binary tree.
+ */
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
+
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
